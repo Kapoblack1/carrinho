3decf18fa1370dbb85b2c8af16489dd09cab6156
Kapoblack1 <Alepy1@hotmail.com>
2024-03-10 02:52:32 +0100

    third commit

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import RegisterScreen from '../screens/RegisterScreen';
 import LoginScreen from '../screens/LoginScreen';
 import CarrinhosScreen from '../screens/CarrinhosSrceen';
+import DetailsCarrinhoScreen from '../screens/DetailsCarrinho';
 
 
 
@@ -14,12 +15,13 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="CarrinhosScreen"
+        initialRouteName="DetailsCarrinhoScreen"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="CarrinhosScreen" component={CarrinhosScreen} />
+        <Stack.Screen name="DetailsCarrinhoScreen" component={DetailsCarrinhoScreen} />
         
       </Stack.Navigator>
     </NavigationContainer>
